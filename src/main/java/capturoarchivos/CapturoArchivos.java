8c3fdedd3a3b0f2d9d6882b613fad668c89552d4
Carol <carol280696@gmail.com>
2022-11-22 16:52:20 -0500

    segundo envio carol

--- src/main/java/capturoarchivos/CapturoArchivos.java
+++ src/main/java/capturoarchivos/CapturoArchivos.java
@@ -1,49 +1,44 @@
 package capturoarchivos;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CapturoArchivos {
-   
 
-       public static void leerArchivo(String nombreArchivo) {
-           var archivo = new File(nombreArchivo);
-            String linea = "";
-            String separador = ",";
-            
-            try {
-            var entrada = new BufferedReader(new FileReader(archivo));
-            
-                while ((linea = entrada.readLine()) != null) 
-                {
-                    String[] cliente = linea.split(separador);
-                    String nombre = cliente[0];
-                    String apellido = cliente[1];
-                    String identificacion = cliente[2];
-                    String celular = cliente [3];
-                    String referencia = cliente [4];
-                    String correo = cliente [5];
-                    String valor_pagar = cliente [6];
-                    String fecha_venci = cliente [7];
-                    
-                    
-                    System.out.println("nombre = " + nombre);
-                    System.out.println("Apellido = " + apellido);
-                    System.out.println("Identificación = " + identificacion);
-                    System.out.println("N° cedlular = " + celular);
-                    System.out.println("N° Referencia = " + referencia);
-                    System.out.println("Correo electronico = " + correo);
-                    System.out.println("Valor a pagar= " + valor_pagar);
-                    System.out.println("Fecha de vencimiento = " + fecha_venci);
-                    System.out.println("Cliente -----------------------------");
-             
-                  }
-            } catch (IOException e) {
-                e.printStackTrace();
+    public static ArrayList<Persona> obtener() {
+        final String archivo = "C:\\Users\\CAROL VELEZ\\Documents\\prueba\\carol1.txt";
+        String linea;
+        String separador = ",";
+        ArrayList<Persona> personas = new ArrayList<>();
+        FileReader entrada = null;
+        BufferedReader entrada1 = null;
+
+        try {
+            entrada = new FileReader(archivo);
+            entrada1 = new BufferedReader(entrada);
+
+            while ((linea = entrada1.readLine()) != null) {
+                String[] cliente = linea.split(separador);
+                personas.add(new Persona(cliente[0], cliente[1], Integer.valueOf(cliente[2]), Integer.valueOf(cliente[3])));
+               
+
             }
+        } catch (IOException e) {
+            System.out.println("Excepción leyendo archivo: " + e.getMessage());
+        } finally {
+            try {
+                if (entrada != null) {
+                    entrada.close();
+                }
+                if (entrada1 != null) {
+                    entrada1.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Excepción cerrando: " + e.getMessage());
+            }
+            return personas;
         }
     }
-
-
+}
--- src/main/java/test/TestProyecto.java
+++ src/main/java/test/TestProyecto.java
@@ -1,16 +1,28 @@
-
 package test;
 
-import static capturoarchivos.CapturoArchivos.leerArchivo;
-
+//import static capturoarchivos.CapturoArchivos.leerArchivo;
+import static capturoarchivos.CapturoArchivos.obtener;
+import capturoarchivos.Persona;
+import java.util.ArrayList;
 
 public class TestProyecto {
-     public static void main(String[] args) {
-         var nombreArchivo = "C:\\Users\\CAROL VELEZ\\Documents\\prueba\\primero.txt";
 
-        leerArchivo(nombreArchivo);
-         
-         
-     }
-    
+    public static void main(String[] args) {
+        // var nombreArchivo = "C:\\Users\\CAROL VELEZ\\Documents\\prueba\\primero.csv";
+
+//        leerArchivo(nombreArchivo);
+        ArrayList<Persona> personas = obtener();
+        // Podemos imprimirlas...
+        System.out.println(personas);
+        // O recorrerlas
+        for (int x = 0; x < personas.size(); x++) {
+            Persona persona = personas.get(x);
+           System.out.println("Tenemos una persona con nombre " + persona.getNombre() + " tipo documento " 
+                   + persona.getTipo_documento() + " documento " + persona.getNumero_documento() 
+                   + " y referencia de pago " + persona.getReferencia_pago());
+        }
+
+    }
+
 }
+
